2cf69e0782bde432d6593f3ecbc036b80142aa3e
Dasha Yushkova <d.yushkova@andersenlab.com>
2020-12-24 22:41:17 +0300

    improved 'calculate age' unit test

--- unit_test/src/test/java/AgeTest.java
+++ unit_test/src/test/java/AgeTest.java
@@ -1,15 +1,33 @@
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class AgeTest {
 
-    @Test(description = "Checking that the applied age is more than 18")
-    public void testCalculateAge() {
-        LocalDate birthDate = LocalDate.of(1988, 9, 26);
-        int actualAge = Age.calculateAge(birthDate);
-        assertTrue(actualAge >= 18);
+    private LocalDate invalidBirthday, validBirthday;
+
+    @BeforeMethod(description = "Preparing valid & invalid data")
+    public void prepareTestData() {
+        validBirthday = getYearsAgo(18);
+        invalidBirthday = getYearsAgo(17);
+    }
+
+    @Test(description = "Checking if the applied age is more than 18")
+    public void testValidateAge() {
+        assertFalse(Age.calculateAge(invalidBirthday) >= 18);
+        assertTrue(Age.calculateAge(validBirthday) >= 18);
+    }
+
+    private static LocalDate getYearsAgo(int yearsAgo){
+        Calendar date = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        date.set(Calendar.YEAR, date.get(Calendar.YEAR) - yearsAgo);
+        return LocalDate.parse(formatter.format(date.getTime()));
     }
 }
